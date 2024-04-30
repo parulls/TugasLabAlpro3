e2bcf2eb4418ca0f9303e6531e8b1f7ca6a2da47
Parulian Dwi Reslia Simbolon <paruliandwireslia22@gmail.com>
2024-04-30 20:58:29 +0700

    fungsi rekursif

--- 032_Parulian_rekursif.cpp
+++ 032_Parulian_rekursif.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int rekursifEuclianGCD(int m, int n){
+    if (n==0)
+        return m;
+    else 
+        return rekursifEuclianGCD (n,m%n);
+}
 main (){
-
+    system("cls");
+    int a,b;
+    cout<<"Masukkan nilai bil1 : ";cin>>a;
+    cout<<"Masukkan nilai bil2 : ";cin>>b;
+    int fpb=rekursifEuclianGCD(a,b);
+    cout<<"FPB : "<<fpb<<endl;
     return 0;
 }
