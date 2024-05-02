2fa7cab5b55048429d7678822998ece169033132
Parulian Dwi Reslia Simbolon <paruliandwireslia22@gmail.com>
2024-05-02 19:52:34 +0700

    menambahkan penjelasan

--- 032_Parulian_rekursif.cpp
+++ 032_Parulian_rekursif.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int rekursifEuclianGCD(int m, int n){
-    if (n==0)
-        return m;
-    else 
-        return rekursifEuclianGCD (n,m%n);
+int rekursifEuclideanGCD(int m, int n){
+    if (n==0)//mengecek apakah n bernilai sama dengan 0 atau tidak 
+        return m;//jika ya, maka kembalikan nilai m sebagai fpb 
+    else //jika n tidak sama dengan 0
+        return rekursifEuclianGCD (n,m%n);//mengembalikan nilai dari fungsi rekursiEuclideanGCD tetapi dengan parameter n dan sisa pembagian m dengan n
 }
 main (){
     system("cls");
     int a,b;
     cout<<"Masukkan nilai bil1 : ";cin>>a;
     cout<<"Masukkan nilai bil2 : ";cin>>b;
-    int fpb=rekursifEuclianGCD(a,b);
+    int fpb=rekursifEuclideanGCD(a,b);//deklarasi variabel fpb dan inisialisasi dengan nilai kembalian dari fungsi rekursifEuclideanGCD
     cout<<"FPB : "<<fpb<<endl;
     return 0;
 }
